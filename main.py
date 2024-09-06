a5c9da6eaf9fd771ba5ba9b289bb73aa44675374
Devansh Mittal <ddevansh82@gmail.com>
2024-09-06 14:15:09 +0530

    routes file added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from routes import routes
 
 app = Flask(__name__)
 
@@ -7,4 +8,5 @@ def home():
     return 'Hello, World!'
 
 if __name__ == '__main__':
+    app.register_blueprint(routes)
     app.run(debug=True)
