c4ffd8f7ef592b9c975e79b6b08ddb4487895c51
Devansh Mittal <ddevansh82@gmail.com>
2024-09-07 13:57:09 +0530

    new route

--- routes/routes.py
+++ routes/routes.py
@@ -5,3 +5,7 @@ ch = Blueprint("test", __name__, url_prefix="/test")
 @ch.route("/data")
 def process_data():
     return jsonify({"message": "ok"})
+
+@ch.route('/test')
+def method_name():
+    return  jsonify({"message": "ok"})
